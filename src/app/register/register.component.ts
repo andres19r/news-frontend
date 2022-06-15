67ca80094cdb6e0ed632cb3c9887eaa9f3ed8404
Andres Rivero <andres19rivero@gmail.com>
2022-06-15 11:43:17 -0400

    login & register components

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  username: string = ''
+  email: string = ''
+  password: string = ''
+  confirmPassword: string = ''
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.username);
+    console.log(this.email);
+  }
+
+}
